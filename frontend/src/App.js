0c5063a4d59d7a03b2889e30cb121c0001b45537
WTS25kt <ishii25kenta@gmail.com>
2024-07-12 17:13:01 +0900

    バックエンドでアップロードされた画像を保存
    pythonでバックエンドを実装し、アップロードボタンを押すとbackend/upload/に保存される機能を追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
     formData.append("file", file);
 
     try {
-      const response = await fetch("http://localhost:5000/upload", {
+      const response = await fetch("http://127.0.0.1:5000/upload", {
         method: "POST",
         body: formData,
       });
